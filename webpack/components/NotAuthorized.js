bda458695bb44e25de377e043e520132d079b802
dave <djungst@gmail.com>
2016-10-18 11:41:27 -0600

    Authorized routes

---
+++ webpack/components/NotAuthorized.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotAuthorized = () => (
+  <div>
+    You are not authorized for this action
+  </div>
+);
+
+export default NotAuthorized;
--- webpack/routes.js
+++ webpack/routes.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute, browserHistory } from 'react-router';
+import { UserAuthWrapper } from 'redux-auth-wrapper';
 import App from './containers/App';
 import NoMatch from './components/NoMatch';
+import NotAuthorized from './components/NotAuthorized';
 import AuthenticatedRoutes from './components/AuthenticatedRoutes';
+
+const SiteAdminAccess = UserAuthWrapper({
+  authSelector: state => state.user,
+  predicate: user => { return user.role === 'site_admin' },
+  redirectAction: () => browserHistory.push('/401'), //I would like to change this to handle a redirect back to where they were
+  wrapperDisplayName: 'UserIsSiteAdmin'
+})
+
+//We could possible have admins who are not site admins but site admins should also be able to view this content
+const AdminAccess = UserAuthWrapper({
+  authSelector: state => state.user,
+  predicate: user => {
+    let regex = new RegExp(/(site_)?admin/)
+    return regex.test(user.role)
+  },
+  redirectAction: () => browserHistory.push('/401'), //I would like to change this to handle a redirect back to where they were
+  wrapperDisplayName: 'UserIsAdmin'
+})
+
+const SiteAdminRoutes = SiteAdminAccess( (props) => props.children )
 
 export default (
   <Route>
     <Route path="/" component={App}>
       <IndexRoute component={AuthenticatedRoutes}>
+        <Route component={SiteAdminRoutes}>
+         {/* Site admin only routes */}
+        </Route>
       </IndexRoute>
       {/* NO ROUTES BELOW THIS LINE */}
+      <Route path="/401" status={401} component={NotAuthorized} />
       <Route path="*" status={404} component={NoMatch}/>
     </Route>
   </Route>
